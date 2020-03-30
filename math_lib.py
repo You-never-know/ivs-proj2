4e4ed86d861f7ec8bf84f8500921ce80fa1264b8
You-never-know <62153084+You-never-know@users.noreply.github.com>
2020-03-30 22:43:45 +0200

    Add files via upload
    Kostra matematických funkcií

---
+++ math_lib.py
@@ -0,0 +1,26 @@
+
+def sucet(x1, x2):
+	return x1 + x2
+
+def rozdiel(x1,x2):
+	return x1 - x2
+
+def nasobenie(x1,x2):
+	return x1 * x2
+
+def delenie(x1, x2):
+
+	return 0
+
+def faktorial(x):
+	return 0
+
+def prir_mocnina(x, exponent):
+	return 0
+
+def odmocnina(x, zaklad):
+	return 0
+
+def modulo(x1, x2):
+
+	return 0
